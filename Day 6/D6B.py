039e3e0bf414a04c94472f6a4f145c85aa40834c
An Vu <anvu2211@gmail.com>
2023-12-06 20:56:33 +1100

    Update D6B.py

--- Day 6/D6B.py
+++ Day 6/D6B.py
@@ -10,8 +10,8 @@ d = ip.split("\n")[1].replace(" ","").split(":")[1:]
 def cal(t, d):
     x1 = ((t-math.sqrt(t*t - 4*d))/2)
     x2 = (t+math.sqrt(t*t - 4*d))/2
-    return math.ceil(x1), math.floor(x2)
+    return math.floor(x1), math.ceil(x2)
 
 for i in range(len(t)):
     x1, x2 = cal(int(t[i]), int(d[i]))
-    print(x2-x1+1)
+    print(x2-x1-1)
