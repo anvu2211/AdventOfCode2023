df74e635c72e21010edd82cbda9e768f8625be17
An Vu <anvu2211@gmail.com>
2023-12-09 16:51:09 +1100

    Add day 9

---
+++ Day 9/D9A.py
@@ -0,0 +1,28 @@
+f = open("input.txt", "r")
+ip = f.read()
+#ip = input()
+
+
+def check(nums):
+    for i in nums:
+        if i != 0:
+            return False
+    return True
+def cal(nums):
+    nums = [int(x) for x in nums]
+    ss = [nums]
+    level = 0
+    while not check(ss[level]):
+        ss.append([])
+        for i in range(len(ss[level])-1):
+            ss[level+1].append(ss[level][i+1]-ss[level][i])
+        level += 1
+    res = 0
+    for i in ss:
+        res += i[-1]
+    return res
+
+result = 0
+for line in ip.split("\n"):
+    result += cal(line.split())
+print(result)
